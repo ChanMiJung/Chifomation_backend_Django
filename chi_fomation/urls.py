f4348d50861842008d942fc0d7d57ea18c616723
ChanMi Jung <prinpice@naver.com>
2022-03-26 22:57:47 +0900

    [Update]Fix media error

--- chi_fomation/urls.py
+++ chi_fomation/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 from main import views as main_views
 
 urlpatterns = [
