21910569176cef07678f31daa93008d4ae083877
ChanMi Jung <prinpice@naver.com>
2022-03-28 16:57:50 +0900

    [Update]Modify static url

--- chi_fomation/urls.py
+++ chi_fomation/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('/', include('main.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
