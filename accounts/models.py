001e089b90abba28edc1e23496025171ea3e379a
ChanMi Jung <prinpice@naver.com>
2022-04-10 14:21:56 +0900

    [Update]Modify migration

--- accounts/models.py
+++ accounts/models.py
@@ -4,11 +4,7 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=40)
-
 class User(AbstractUser):
     userID = models.CharField(max_length=20)    
     phoneNum = models.CharField(max_length=13, blank=True)
+    nickname = models.CharField(max_length=40)
